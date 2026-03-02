fe6541428f8e909314c4cccc6b8243458b4da373
Moisés Alfonso Guerrero Jiménez <moises.guerrero@lovelytics.com>
2026-03-01 21:49:06 -0500

    Updated 2 for test

--- src/release/table2.sql
+++ src/release/table2.sql
@@ -1,8 +1,11 @@
 -- formatted user:mi-id-example-2
+-- liquid syntax: true
 CREATE TABLE IF NOT EXISTS example_catalog.example_schema.table_example2 (
     id INT,
     name STRING,
-    crd_dt DATE
+    crd_dt DATE,
+    crd_by STRING,
+    upd_dt DATE
 )
 STORED AS DELTA
 PARTITIONED BY (crd_dt)
